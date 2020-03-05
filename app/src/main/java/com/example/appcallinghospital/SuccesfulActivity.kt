7e7cc77ad542485a9791fe2cfb6c081e2350c2ee
NIConqueror <npokhodenko@yandex.by>
2020-03-05 14:02:53 +0300

    add win.layout/succesfulregisration

--- app/src/main/java/com/example/appcallinghospital/SuccesfulActivity.kt
+++ app/src/main/java/com/example/appcallinghospital/SuccesfulActivity.kt
@@ -1,19 +1,12 @@
 package com.example.appcallinghospital
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class SuccesfulActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_succesful)
-    }
-
-    fun call_hospit(viev: View){
-        val callIntent = Intent (this, LoginActivity::class.java)
-        startActivity(callIntent)
     }
 }
