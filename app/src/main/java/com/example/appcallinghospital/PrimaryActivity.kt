e6ee331ea66540eaf0754c9179b9ddff9e60da92
NIConqueror <npokhodenko@yandex.by>
2020-03-12 13:16:11 +0300

    update info main.kt

--- app/src/main/java/com/example/appcallinghospital/PrimaryActivity.kt
+++ app/src/main/java/com/example/appcallinghospital/PrimaryActivity.kt
@@ -1,19 +1,12 @@
 package com.example.appcallinghospital
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class PrimaryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_primary)
-    }
-
-    fun call_hospit(viev: View){
-        val callIntent = Intent (this, LoginActivity::class.java)
-        startActivity(callIntent)
     }
 }
